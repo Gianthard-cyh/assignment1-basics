fix: correct total_steps computation, correct param init method

cs336_basics/model/embedding.py
@@ -15,7 +15,7 @@ class Embedding(nn.Module):
         """
         super().__init__()
         self.weight = nn.Parameter(torch.empty(num_embeddings, embedding_dim, device=device))
-        torch.nn.init.trunc_normal_(self.weight)
+        torch.nn.init.trunc_normal_(self.weight, std=1, a=-3, b=3)
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         """

cs336_basics/model/linear.py
@@ -5,6 +5,7 @@ Linear Module
 from einops import einsum
 import torch
 import torch.nn as nn
+import math
 
 
 class Linear(nn.Module):
@@ -20,7 +21,9 @@ class Linear(nn.Module):
         """
         super().__init__()
         self.weight = nn.Parameter(torch.empty(out_features, in_features, device=device, dtype=dtype))
-        torch.nn.init.trunc_normal_(self.weight)
+
+        sigma = math.sqrt(2.0 / (in_features + out_features))
+        nn.init.trunc_normal_(self.weight, std=sigma, a=-3 * sigma, b=3 * sigma)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """

cs336_basics/model/rmsnorm.py
@@ -14,8 +14,7 @@ class RMSNorm(nn.Module):
             dtype (torch.dtype | None): 参数的数据类型
         """
         super().__init__()
-        self.weight = nn.Parameter(torch.empty(d_model, device=device, dtype=dtype))
-        torch.nn.init.zeros_(self.weight)
+        self.weight = nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
         self.d_model = d_model
         self.eps = eps
         self.dtype = dtype

cs336_basics/train/trainer.py
@@ -67,7 +67,7 @@ class LMTrainer:
         self.train_dataset = np.load(self.train_dataset_path, mmap_mode="r")
         self.val_dataset = np.load(self.val_dataset_path, mmap_mode="r")
         print(f"- Train dataset: {len(self.train_dataset)} tokens")
-        self.total_steps = int(math.ceil(self.trainer_config.max_samples / self.trainer_config.train_batch_size))
+        self.total_steps = int(math.ceil(self.trainer_config.max_samples / self.trainer_config.train_batch_size / self.trainer_config.context_length)) # 竟然旺季处context_length了！
         self.cur_step = 1
         print(f"- total_steps: {self.total_steps}")
         for p in self.model.named_parameters():
@@ -83,7 +83,7 @@ class LMTrainer:
             self.trainer_config.context_length,
             self.trainer_config.device,
         )
-        lr = cosine_lr_schedule(self.cur_step, self.trainer_config.lr, 0.0, 100, self.total_steps)
+        lr = cosine_lr_schedule(self.cur_step, self.trainer_config.lr, 0.0, 1 / 10 * self.total_steps, self.total_steps)
         for param_group in self.optimizer.param_groups:
             param_group["lr"] = lr
         logits = self.model(input_ids)

scripts/run_train.py
@@ -35,10 +35,10 @@ def run_training():
     trainer_cfg = TrainerConfig(
         lr=1e-4,
         weight_decay=0.01,
-        beta1=0.9,
-        beta2=0.95,
+        beta1=0.99,
+        beta2=0.999,
         max_samples=320000000,
-        train_batch_size=512,
+        train_batch_size=32,
         val_batch_size=16,
         context_length=256,
         device="cuda" if torch.cuda.is_available() else "cpu",